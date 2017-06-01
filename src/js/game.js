89fb000b7bafcf07f4ded9aa1f9004e599974218
Patrick Hahn <patricksilashahn@gmx.net>
2017-06-01 16:13:39 +0200

    specialized balloon rendering preparations for specialized balloon effects

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,6 @@
 class StateManager {
-	constructor(canvas) {
-		this.canvas = canvas;
+	constructor(game) {
+		this.game = game;
 		this.states = {};
 		this.stack = [];
 	}
@@ -17,12 +17,12 @@ class StateManager {
 			this.stack[this.stack.length - 1].onPause();
 
 		}
-		this.states[id].onInit(this.canvas);
+		this.states[id].onInit(this.game);
 		this.stack.push(this.states[id]);
 	}
 
 	popState() {
-		if(this.stack.length === 1) {
+		if (this.stack.length === 1) {
 			throw new Error('No state below this');
 		}
 		this.stack[this.stack.length - 1].onDisable();
@@ -43,8 +43,30 @@ class StateManager {
 	}
 }
 
+class EventBus
+{
+	constructor() {
+		this.handlers = {};
+	}
+
+	publish(eventType, event) {
+		if(eventType in this.handlers) {
+			this.handlers[eventType].forEach(handler => handler(event));
+		}
+	}
+
+	subscribe(eventType, handler) {
+		if(this.handlers[eventType]) {
+			this.handlers[eventType].push(handler);
+		} else {
+			this.handlers[eventType] = [handler];
+		}
+
+	}
+}
+
 class State {
-	onInit(canvas) {
+	onInit(game) {
 	}
 
 	onDisable() {
@@ -59,7 +81,7 @@ class State {
 	update(dt) {
 	}
 
-	draw(ctx, canvas) {
+	draw(ctx) {
 	}
 
 	handleEvent(eventType, event) {
@@ -83,8 +105,8 @@ class BalloonState extends State {
 		this.popped = 0;
 	}
 
-	onInit(canvas) {
-		this.canvas = canvas;
+	onInit(game) {
+		this.game = game;
 		this.popped = 0;
 		this.balloons = [];
 		this.spawnCountdown = 0;
@@ -94,13 +116,14 @@ class BalloonState extends State {
 		this.spawnCountdown -= dt;
 		if (this.spawnCountdown <= 0) {
 			this.spawnCountdown = 1000 + (Math.random() * 500 - 250);
-			this.balloons.push(new Balloon(this.colors[Math.floor(Math.random() * this.colors.length)], this.canvas));
+			this.balloons.push(new BombBalloon(this.colors[Math.floor(Math.random() * this.colors.length)], this.game));
 		}
 		this.balloons.forEach(balloon => balloon.update(dt));
+		this.balloons = this.balloons.filter(balloon => !balloon.canRemove());
 	}
 
-	draw(ctx, canvas) {
-		this.balloons.forEach(balloon => balloon.draw(ctx, canvas));
+	draw(ctx) {
+		this.balloons.forEach(balloon => balloon.draw(ctx));
 		ctx.font = '25px Arial';
 		ctx.fillStyle = '#000';
 		ctx.fillText('' + this.popped, 10, 35);
@@ -121,29 +144,34 @@ class BalloonState extends State {
 				}).filter(touch => {
 					return touch.x > 0 && touch.x < this.canvas.width && touch.y > 0 && touch.y < this.canvas.height;
 				}).forEach(touch => {
-					this.balloons = this.balloons.filter(balloon => !balloon.checkHit(touch));
+					this.balloons.filter(balloon => balloon.checkHit(touch)).forEach(balloon => {
+						balloon.explode();
+						this.popped++;
+					});
 				});
 				break;
 			case 'click':
-				this.balloons = this.balloons.filter(balloon => {
-					const boundingRect = this.canvas.getBoundingClientRect();
+				this.balloons.filter(balloon => {
+					const boundingRect = this.game.canvas.getBoundingClientRect();
 					const point = {
 						x: event.pageX - boundingRect.left,
 						y: event.pageY - boundingRect.top
 					};
-					return !balloon.checkHit(point);
+					return balloon.checkHit(point);
+				}).forEach(balloon => {
+					balloon.explode();
+					this.popped++;
 				});
 				break;
 		}
-		this.popped += lenghtBefore - this.balloons.length;
 	}
 }
 
 class Balloon {
-	constructor(color, canvas) {
+	constructor(color, game) {
 		this.canvasSize = {
-			x: canvas.width,
-			y: canvas.height
+			x: game.canvas.width,
+			y: game.canvas.height
 		};
 		this.size = {
 			x: 35,
@@ -158,10 +186,17 @@ class Balloon {
 			x: Math.random() * 16 - 8,
 			y: -75 + (Math.random() * 16 - 8)
 		};
+
+		//this.pos = {x:100,y:100}; this.vel={x:0,y:0};
+		//console.log(this.vel);
+		this.popping = false;
+		this.poppingTimer = 0.05;
+		this.poppingRate = {x: 100, y: 100};
 	}
 
 	update(dt) {
 		//console.log(this.pos.x, this.pos.y);
+		//console.log(this.popping);
 		if (this.pos.x + this.size.x > this.canvasSize.x || this.pos.x - this.size.x < 0) {
 			this.vel.x = -this.vel.x;
 		}
@@ -169,39 +204,106 @@ class Balloon {
 		//console.log('update balloon', this);
 		this.pos.y += this.vel.y * (dt / 1000);
 		this.pos.x += this.vel.x * (dt / 1000);
+
+		if (this.popping && this.poppingTimer > 0) {
+			this.poppingTimer -= dt / 1000;
+			this.size.x += this.poppingRate.x * (dt / 1000);
+			this.size.y += this.poppingRate.y * (dt / 1000);
+			this.vel = {x:0,y:0};
+		}
+
 	}
 
 	draw(ctx, canvas) {
-		//console.log('draw balloon', this);
+		if (this.canRemove()) { // don't draw if we should be gone already
+			return;
+		}
+
+		const x = this.pos.x;
+		const y = Math.floor(this.pos.y);
+		const sx = this.size.x;
+		const sy = Math.floor(this.size.y);
+
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
 		ctx.strokeStyle = '#FFF';
 		ctx.lineWidth = 3;
-		ctx.ellipse(this.pos.x, this.pos.y, this.size.x, this.size.y, 0, 0, 2 * Math.PI);
+		ctx.ellipse(x, y, sx, sy, 0, 0, 2 * Math.PI);
 		ctx.stroke();
-		ctx.moveTo(this.pos.x, this.pos.y + this.size.y - 3);
-		ctx.lineTo(this.pos.x - 7, this.pos.y + this.size.y + 7);
-		ctx.lineTo(this.pos.x + 7, this.pos.y + this.size.y + 7);
+		ctx.moveTo(x, y + sy - 3);
+		ctx.lineTo(x - 7, y + sy + 7);
+		ctx.lineTo(x + 7, y + sy + 7);
 		ctx.lineWidth = 1;
 		ctx.stroke();
 		ctx.fill();
-
 		ctx.closePath();
-		//ctx.fillRect(this.pos.x, this.pos.y, 100, 100);
 	}
 
 	checkHit(point) {
 		return ((point.x - this.pos.x) * (point.x - this.pos.x)) /
-						(this.size.x * this.size.x + 4) +
+			((this.size.x+5) * (this.size.x+5)) +
 			((point.y - this.pos.y) * (point.y - this.pos.y)) /
-						(this.size.y * this.size.y + 4) <= 1;
+			((this.size.y+5) * (this.size.y+5)) <= 1;
+	}
+
+	explode() {
+		this.popping = true;
+	}
+
+	canRemove() {
+		return this.popping && this.poppingTimer < 0 || this.pos.y + this.size.y + 7 < 0;
+	}
+}
+
+class SpeedupBalloon extends Balloon
+{
+	draw(ctx, game) {
+		super.draw(ctx, game);
+		ctx.beginPath();
+		ctx.moveTo(this.pos.x - 10, this.pos.y - 10);
+		ctx.lineTo(this.pos.x, this.pos.y);
+		ctx.lineTo(this.pos.x - 10, this.pos.y + 10);
+
+		ctx.moveTo(this.pos.x, this.pos.y - 10);
+		ctx.lineTo(this.pos.x + 10, this.pos.y);
+		ctx.lineTo(this.pos.x, this.pos.y + 10);
+		ctx.strokeStyle = 'white';
+		ctx.lineWidth = 5;
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
+
+class BombBalloon extends Balloon
+{
+	constructor(color, game) {
+		super(color, game);
+
+		this.poppingRate = {x: 2000, y: 2500};
+		this.poppingTimer = 0.45;
+	}
+
+	draw(ctx) {
+		super.draw(ctx);
+
+		const bombRadius = this.size.x - 20;
+		const fuseEnd = {x: this.pos.x + this.size.x - 20, y: this.pos.y - this.size.y + 15};
+
+		ctx.beginPath();
+		ctx.arc(this.pos.x, this.pos.y, bombRadius, 0, 2*Math.PI);
+		ctx.fillStyle = 'white';
+		ctx.moveTo(this.pos.x, this.pos.y - bombRadius);
+		ctx.lineTo(fuseEnd.x, fuseEnd.y);
+		ctx.lineWidth = 5;
+		ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
 	}
 }
 
 class Game {
-	constructor(canvas, stateManager) {
+	constructor(canvas) {
 		this.lastTimestamp = null;
-		this.stateManager = stateManager;
 		this.canvas = canvas;
 
 		this.canvas.addEventListener('touchstart', (touchevent) => {
@@ -212,6 +314,12 @@ class Game {
 			mouseevent.preventDefault();
 			this.stateManager.notifyTop('click', mouseevent);
 		});
+
+		this.eventbus = new EventBus();
+
+		this.stateManager = new StateManager(this);
+		this.stateManager.registerState('balloon', new BalloonState());
+		this.stateManager.pushState('balloon');
 	}
 
 	update(timestamp) {
@@ -232,24 +340,18 @@ class Game {
 (function () {
 	let canvas = document.createElement('canvas');
 	canvas.height = window.innerHeight;
-	canvas.width = 500;
+	canvas.width = Math.min(window.innerWidth, 500);
+	canvas.style.width = Math.min(window.innerWidth, 500) + 'px';
 	canvas.id = 'canvas';
 	document.getElementsByClassName('canvas-wrapper')[0].appendChild(canvas);
 	document.getElementsByClassName('canvas-wrapper')[0].style.height = window.innerHeight;
 
+	let game = new Game(canvas);
 
-	let stateManager = new StateManager(canvas);
-	stateManager.registerState('balloon', new BalloonState());
-	stateManager.pushState('balloon');
-	let game = new Game(canvas, stateManager);
-	let faketime = 0;
 	const draw = (timestamp) => {
-		//console.log(timestamp);
 		game.update(timestamp);
 		game.draw();
 		requestAnimationFrame(draw);
-		//setTimeout(() => draw(faketime += 1./25), 1./25);
 	};
-	//setTimeout(() => draw(faketime += 1./25), 1./25);
-	requestAnimationFrame(draw);
+	setTimeout(() => requestAnimationFrame(draw), 1500);
 })();
